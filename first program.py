e85e5538979c7874ba0cfab6b7fdb8da1b177088
Tejasbadgujarmlenginear <tejasbadgujar0203@gmail.com>
2025-11-08 19:21:03 +0530

    Meri Python journey ka pehla commit

---
+++ first program.py
@@ -0,0 +1,8 @@
+
+print("Hi My Name Is Tejas Badgujar And This Is My First Program")
+print("Than My Goal Is One Year Hardwork")
+print("And Better Results Achieved")
+print("So I Staring My Challenge Is 08-11-2025")
+print("And Complete My Challenge In 08-11-2026")
+
+
